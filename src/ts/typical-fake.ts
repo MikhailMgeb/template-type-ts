fa7bb9c1481a2a85d27992a70801ee046104092a
Michael Mgebrishvili <mmd920802@gmail.com>
2024-01-06 14:12:22 +0300

    typical-fake fail

--- src/ts/typical-fake.ts
+++ src/ts/typical-fake.ts
@@ -1,9 +1,11 @@
 // Типичные фейки
 // Возьмите апи https://dummyjson.com/products. Смотрите в возвращаемые ручкой данные и напишите их тип.
 
+type Category = 'smartphones' | 'laptops' | 'fragrances' | 'skincare' | 'groceries' | 'home-decoration';
+
 type TypeProducts = {
     brand: string;
-    category: string;
+    category: Category;
     description: string;
     discountPercentage: number;
     id: number;
@@ -13,19 +15,19 @@ type TypeProducts = {
     rating: string;
     stock: string
     thumbnail: string;
-}
+};
 
 type TypeResponse = {
     limit: number;
     products: TypeProducts;
     skip: number;
     total: number;
-}
+};
 
 function getApiData(): Promise<TypeResponse> {
     return fetch('https://dummyjson.com/products')
-    .then((response) => response.json())
+        .then((response) => response.json())
 }
 
-getApiData().then((data) => console.log(data.products.price))
+getApiData().then((data) => console.log(data.products.price));
 
